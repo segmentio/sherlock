a45039464d53f7fdd62bc82d36880b3b709ac933
Dominic Barnes <dominic@segment.io>
2015-02-17 20:22:50 +0000

    refactoring the evaluate logic to be more efficient

--- test/detective.js
+++ test/detective.js
@@ -25,69 +25,63 @@ describe('detective.analyze(url)', function () {
       var url = fixture('google-analytics/classic.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'Google Analytics', { trackingId: 'UA-XXXXX-X' })
+      assert.deepEqual(results, { 'Google Analytics': { trackingId: 'UA-XXXXX-X' } });
     });
 
     it('should detect universal script', function *() {
       var url = fixture('google-analytics/universal.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'Google Analytics', { trackingId: 'UA-XXXX-Y' })
+      assert.deepEqual(results, { 'Google Analytics': { trackingId: 'UA-XXXX-Y' } });
     });
   });
+
   describe('Google Tag Manager', function () {
     it('should detect script', function *() {
       var url = fixture('google-tag-manager/index.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'Google Tag Manager', { containerId: 'GTM-KF7RSC' })
+      assert.deepEqual(results, { 'Google Tag Manager': { containerId: 'GTM-KF7RSC' } });
     });
   });
+
   describe('Mixpanel', function () {
     it('should detect script', function *() {
       var url = fixture('mixpanel/index.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'Mixpanel', { token: 'YOUR TOKEN' })
+      assert.deepEqual(results, { 'Mixpanel': { token: 'YOUR TOKEN' } });
     });
   });
+
   describe('FullStory', function () {
     it('should detect script', function *() {
       var url = fixture('fullstory/index.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'FullStory', { fsOrg: '<FS_ORG>' })
+      assert.deepEqual(results, { 'FullStory': { fsOrg: '<FS_ORG>' } });
     });
   });
+
   describe('TrackJS', function () {
     it('should detect script', function *() {
       var url = fixture('trackjs/index.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'TrackJS', { token: 'YOUR_TOKEN' })
+      assert.deepEqual(results, { 'TrackJS': { token: 'YOUR_TOKEN' } });
     });
   });
+
   describe('KISSmetrics', function () {
     it('should detect script', function *() {
       var url = fixture('kissmetrics/index.html');
       var results = yield detective.analyze(url);
 
-      detected(results, 'KISSmetrics', { apiKey: '57a0897d0c675651f450229d65ccf4a605112804' })
+      assert.deepEqual(results, { 'KISSmetrics': { apiKey: '57a0897d0c675651f450229d65ccf4a605112804' } });
     });
   });
 });
 
 function fixture(file) {
   return 'http://localhost:8002/' + file;
-}
-
-function detected(results, name, settings) {
-  var found = results.filter(function (result) {
-    return result.name === name;
-  })[0];
-
-  assert.deepEqual(found, {
-    name: name,
-    settings: settings
-  });
 }
