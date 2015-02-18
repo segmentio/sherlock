277097ce93c88f89aa757acfac039253570f4c02
stevenmiller888 <stevenmiller888@me.com>
2015-02-18 10:45:46 -0800

    added keen test

--- test/detective.js
+++ test/detective.js
@@ -71,6 +71,20 @@ describe('detective.analyze(url)', function () {
       assert.deepEqual(results, { 'Heap': { appId: '1535634150' } });
     });
   });
+  
+  describe.only('Keen.io', function () {
+    it('should detect script', function *() {
+      var url = fixture('keen-io/index.html');
+      var results = yield detective.analyze(url);
+
+      assert.deepEqual(results, {
+        'Keen.io': {
+          projectId: '5408d3f4e875963a9a2e60f7',
+          writeKey: 'd258e01d5bb452dcc244b1b458d26b891a72599edae9291d49de6fb365c2a916e35b740331ab1aa0b7a30845d34f0c09f3c560ba59a6f810e49fe13eeeb8dda98b9ee9deae8ca4bf3f6ecd3950ba455ccedd514e1327d2026eac5446168d60723aedcf5a8899c5888b4878527eaafd8c'
+        }
+      });
+    });
+  });
 
   describe('Mixpanel', function () {
     it('should detect script', function *() {
