f2e7096d759680c3d52685047e7c08a03486827f
Dominic Barnes <dominic@segment.com>
2015-03-24 05:19:30 +0000

    removing segment integrations, adding proper tests, updating docs

--- test/index.js
+++ test/index.js
@@ -1,31 +1,45 @@
 
-/**
- * Module dependencies.
- */
-
-var server = require('./server/index.js');
 var assert = require('assert');
 var Sherlock = require('..');
 
-/**
- * Initialize Sherlock.
- */
-
-var sherlock = Sherlock();
-
-/**
- * Sherlock.
- */
-
-describe('sherlock.analyze(url)', function () {
-  this.slow('3s');
-  this.timeout('10s');
-
-  before(function (done) {
-    server.listen(8002, done);
+describe('Sherlock()', function () {
+  it('should be a function', function () {
+    assert.equal(typeof Sherlock, 'function');
   });
 
-  after(function (done) {
-    server.close(done);
+  it('should be a constructor', function () {
+    var sherlock = new Sherlock();
+    assert(sherlock instanceof Sherlock);
   });
+
+  it('should not require the new keyword', function () {
+    var sherlock = Sherlock();
+    assert(sherlock instanceof Sherlock);
+  });
+
+  it('should initialize an empty list of services', function () {
+    var sherlock = new Sherlock();
+    assert.deepEqual(sherlock.services, []);
+  });
+});
+
+describe('Sherlock#use(plugin)', function () {
+  it('should add a single service to the list', function () {
+    var sherlock = new Sherlock();
+    sherlock.use({ name: 'A' });
+    assert.deepEqual(sherlock.services, [ { name: 'A' } ]);
+  });
+
+  it('should add an array of services', function () {
+    var sherlock = new Sherlock();
+    sherlock.use([ { name: 'A' }, { name: 'B' } ]);
+    assert.deepEqual(sherlock.services, [
+      { name: 'A' },
+      { name: 'B' }
+    ]);
+  });
+});
+
+describe('Sherlock#analyze(url)', function () {
+
 });
